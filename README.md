1bfdc3a9c8dccd97c367f135daf142066e4192c6
Harsha S <harsha.s@netapp.com>
2022-08-26 21:56:57 +0530

    describe test

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 # Steps
 1. npx create-react-app react-testing --template typescript
+
+# Description
+1. test.only('test', () => {}) -> only test this methods
+2. test.skip(test', () => {}) -> skip this method
--- react-testing/src/components/greet/greet.test.tsx
+++ react-testing/src/components/greet/greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from '@testing-library/react'
 import { Greet } from './greet'
 
-test('Greet renders correctly', () => {
-    render(<Greet />)
-    const textElement = screen.getByText(/Hello/)
-    expect(textElement).toBeInTheDocument()
-})
+describe('Greet', () => {
+    test('Greet renders correctly', () => {
+        render(<Greet />)
+        const textElement = screen.getByText(/Hello/)
+        expect(textElement).toBeInTheDocument()
+    })
 
-test('Greet renders with a name', () => {
-    render(<Greet name='shastry'/>)
-    const textElement = screen.getByText('Hello shastry')
-    expect(textElement).toBeInTheDocument()
+    describe('Nested', () => {
+        test('Greet renders with a name', () => {
+            render(<Greet name='shastry' />)
+            const textElement = screen.getByText('Hello shastry')
+            expect(textElement).toBeInTheDocument()
+        })
+    })
 })
